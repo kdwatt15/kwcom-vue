fd6853b68df483cec42a3e8dd4ed60a902bf7f18
Kevin Watt <kevinwatt@kpmg.com>
2020-11-30 21:55:23 -0600

    adding images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,17 @@
     <h1>Welcome!</h1>
     <p>I'm glad you got here. Unfortunately, kevin-watt.com is still under development.</p>
     <p>If you would like to connect before the site is ready, please feel to reach out to me on <a href="https://www.linkedin.com/in/kevin-watt/" target="_blank">LinkedIn</a>.</p>
+    <p>In the meantime, please enjoy a photo of the sunrise over Lake Michigan.</p>
+    <img src="@/assets/chicago_sunrise.jpg">
   </div>
 </template>
 
 <style scoped>
 p {
   color: rgb(80, 79, 79);
+}
+img {
+  max-height: 40em;
+  width: auto
 }
 </style>
